56b5d9dd88adcc9a1d79ebb4fde47215a0087eb8
Bing-Chen Tsai <benson1016087@cml18>
2021-03-13 19:55:16 +0800

    Add prototype of train function

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+def train(model, train_loader, val_loader, lr=1e-3, epochs=100, early_stopping=5, model_path=None):
+    '''
+    Training function 
+        model: (nn.Module) BERT-based model
+        train_loader: (DataLoader) Dataloader of training set
+        val_loader: (DataLoader) Dataloader of validation set
+        lr: (float) learning rate
+        epochs: (int) maximum epochs
+        early_stopping: (int) patience of not improved in validation set, 
+                              if set to -1, means not adapt early stopping
+        model_path: (str) if not None, save the best model to the path
+    '''
+
