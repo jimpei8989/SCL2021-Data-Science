b112a56aa711ec7c7157bda0e2215ef12ce1d21d
Bing-Chen Tsai <benson1016087@cml18>
2021-03-16 14:17:41 +0800

    Force output be continuous

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,38 @@
 import torch
 import pandas as pd
 from tqdm import tqdm
+import numpy as np
 
 from reconstruct_tokenized import reconstruct
+
+
+def to_continuous(mask):
+    """
+    mask: [List] value represents probability
+    """
+    def find_max_sub_arr(prob):
+        accumulate = [0]
+        for v in prob:
+            accumulate.append(accumulate[-1] + v)
+
+        max_ans, max_rec = 0, [0, 0]
+        left_min, left_min_idx = 100000, -1
+        for i, v in enumerate(accumulate):
+            if v <= left_min:
+                left_min = v
+                left_min_idx = i
+            if v - left_min > max_ans:
+                max_ans = v - left_min
+                max_rec = [left_min_idx, i]
+
+        return [0] * max_rec[0] + [1] * (max_rec[1] - max_rec[0]) + [0] * (len(prob) - max_rec[1])
+
+    mask = np.array(mask) - 0.5
+    ret = []
+    for m in mask:
+        ret.append(find_max_sub_arr(m))
+
+    return ret
 
 
 def predict(model, tokenizer, test_dataloader, output_csv, device):
@@ -14,8 +44,8 @@ def predict(model, tokenizer, test_dataloader, output_csv, device):
         for batch in tqdm(test_dataloader, ncols=80, desc="Predicting"):
             pred = model(batch["input_ids"].to(device)).to("cpu")
 
-            poi_masks = pred[..., 0].gt(0.5).tolist()
-            street_masks = pred[..., 1].gt(0.5).tolist()
+            poi_masks = to_continuous(pred[..., 0].tolist())
+            street_masks = to_continuous(pred[..., 1].tolist())
 
             for ID, addr, poi_mask, street_mask in zip(
                 batch["id"], batch["address"], poi_masks, street_masks
